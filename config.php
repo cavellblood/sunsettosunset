d7bc7c490235d2e60ea042c08d36dd161230fa86
Cavell Blood <cavellblood@me.com>
2018-09-12 10:58:45 -0400

    remove defaults from `config.php`
    Signed-off-by: Cavell Blood <cavellblood@me.com>

--- config.php
+++ config.php
@@ -5,8 +5,5 @@ return [
      * Show CP section
      */
     'showCpSection' => false,
-    'timezone' => 'America/Chicago',
-    'latitude' => '41.8333925',
-    'longitude' => '-88.0121478'
 
 ];
