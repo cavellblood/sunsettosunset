dbbdaf7673313621545bbda24e582deb112acc0f
Cavell Blood <cavellblood@me.com>
2018-10-07 17:34:03 -0400

    remove PHPDoc comments
    Signed-off-by: Cavell Blood <cavellblood@me.com>

--- variables/SunsetToSunsetVariable.php
+++ variables/SunsetToSunsetVariable.php
@@ -16,65 +16,42 @@ namespace Craft;
 class SunsetToSunsetVariable
 {
 
-    /**
-     * @return mixed
-     */
     public function getMessage()
     {
         return craft()->sunsetToSunset->getMessage();
     }
 
-    /**
-     * @return mixed
-     */
+
     public function getBannerMessage()
     {
         return craft()->sunsetToSunset->getBannerMessage();
     }
 
-    /**
-     * @return mixed
-     */
     public function getClosingTime()
     {
         return craft()->sunsetToSunset->getClosingTime();
     }
 
-    /**
-     * @return mixed
-     */
     public function getOpeningTime()
     {
         return craft()->sunsetToSunset->getOpeningTime();
     }
 
-    /**
-     * @return mixed
-     */
     public function getBannerCssPosition()
     {
         return craft()->sunsetToSunset->getBannerCssPosition();
     }
 
-    /**
-     * @return mixed
-     */
     public function getBannerCssBackgroundColor()
     {
         return craft()->sunsetToSunset->getBannerCssBackgroundColor();
     }
 
-    /**
-     * @return mixed
-     */
     public function getSimulateTime()
     {
         return craft()->sunsetToSunset->getSimulateTime();
     }
 
-    /**
-     * @return mixed
-     */
     public function getTemplate()
     {
         return craft()->sunsetToSunset->getTemplate();
