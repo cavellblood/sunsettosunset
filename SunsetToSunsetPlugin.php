7b6a3da05495e04ebe6b7806c7627c4a98f1fc3b
Cavell Blood <cavellblood@me.com>
2018-10-07 18:34:37 -0400

    add new route to registerCpRoutes
    Signed-off-by: Cavell Blood <cavellblood@me.com>

--- SunsetToSunsetPlugin.php
+++ SunsetToSunsetPlugin.php
@@ -168,6 +168,7 @@ class SunsetToSunsetPlugin extends BasePlugin
     {
         return array(
             'sunsettosunset' => ['action' => 'sunsetToSunset/message/index'],
+            'sunsettosunset/message' => ['action' => 'sunsetToSunset/message/index'],
             'sunsettosunset/location' => ['action' => 'sunsetToSunset/location/index'],
             'sunsettosunset/advanced' => ['action' => 'sunsetToSunset/advanced/index'],
         );
