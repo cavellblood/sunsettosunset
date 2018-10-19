d15408895e3ec40827efdcaa70453b395a10ff89
Cavell Blood <cavellblood@me.com>
2018-10-19 12:04:24 -0400

    move all controllers to main controller
    Signed-off-by: Cavell Blood <cavellblood@me.com>

---
+++ controllers/SunsetToSunsetController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Sunset to Sunset plugin for Craft CMS
+ *
+ * SunsetToSunset Message Controller
+ *
+ * @author    Cavell L. Blood
+ * @copyright Copyright (c) 2017 Cavell L. Blood
+ * @link      cavellblood.com
+ * @package   SunsetToSunset
+ * @since     1
+ */
+
+namespace Craft;
+
+class SunsetToSunsetController extends BaseController
+{
+
+    /**
+     * @var    bool|array Allows anonymous access to this controller's actions.
+     * @access protected
+     */
+    protected $allowAnonymous = array('actionIndex',
+        );
+
+    /**
+     */
+    public function actionSettings()
+    {
+        $settings = craft()->plugins->getPlugin('sunsetToSunset')->getSettings();
+        $this->renderTemplate('sunsettosunset/settings/message', array(
+            'settings' => $settings,
+        ));
+    }
+
+    /**
+     */
+    public function actionSettingsLocation()
+    {
+        $settings = craft()->plugins->getPlugin('sunsetToSunset')->getSettings();
+        $this->renderTemplate('sunsettosunset/settings/location', array(
+            'settings' => $settings,
+        ));
+    }
+
+    /**
+     */
+    public function actionSettingsAdvanced()
+    {
+        $settings = craft()->plugins->getPlugin('sunsetToSunset')->getSettings();
+        $this->renderTemplate('sunsettosunset/settings/advanced', array(
+            'settings' => $settings,
+        ));
+    }
+}
